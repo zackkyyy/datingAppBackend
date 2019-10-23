c6151c03c950b31f6c6a098f149786299dda67fc
Zacky <zacky-kh@live.com>
2019-10-23 15:53:57 +0200

    cross origin

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var app = express();
 
 require('dotenv').config()
 
+
+// cross Origin problem to access routes from any domain
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
